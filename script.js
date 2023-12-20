03f9d8612257763147a4400fc53fa06db24bdcca
Kerosity <9063443+kerosity@users.noreply.github.com>
2023-12-20 00:02:56 -0600

    Add Computer Random Selection Function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function getComputerChoice() {
-
+    const computerChoices = ["Paper", "Scissors", "Rock"];
+    const randomSelection = Math.floor(Math.random()* computerChoices.length);
+    return computerChoices[randomSelection];
 }
+
+console.log(getComputerChoice());
