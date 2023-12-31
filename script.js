553bdbc7950ada9ad24ff4445dbf0cffe7d7645d
Kerosity <9063443+kerosity@users.noreply.github.com>
2023-12-31 13:38:33 -0600

    Clean up naming

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let playerChoice = prompt('Choose Your Destiny: Paper, Scissors, or Rock', '');
+let playerInput = prompt('Choose Your Destiny: Paper, Scissors, or Rock', '');
 let cpuChoice = computerSelection();
+let playerChoice = caseFix(playerInput);
 
 function computerSelection() {
     const computerChoices = ["Paper", "Scissors", "Rock"];
@@ -7,21 +8,21 @@ function computerSelection() {
     return computerChoices[randomSelection];
 }
 
-function caseFix(playerChoice) {
-    let firstLetter = playerChoice[0].toUpperCase();
-    let lowerCase = playerChoice.toLowerCase();
+function caseFix(playerInput) {
+    let firstLetter = playerInput[0].toUpperCase();
+    let lowerCase = playerInput.toLowerCase();
     return firstLetter + lowerCase.slice(1);
 }
-console.log('You chose:', caseFix(playerChoice));
+console.log('You chose:', caseFix(playerInput));
 
 
 function scoring() {
 
     console.log('The computer chose:', cpuChoice);
 
-    if ((caseFix(playerChoice) == 'Rock' && cpuChoice == 'Paper') || (caseFix(playerChoice) == 'Scissors' && cpuChoice == 'Rock') || (caseFix(playerChoice) == 'Paper' && cpuChoice == 'Scissors'))  {
+    if ((playerChoice == 'Rock' && cpuChoice == 'Paper') || (playerChoice == 'Scissors' && cpuChoice == 'Rock') || (playerChoice == 'Paper' && cpuChoice == 'Scissors'))  {
         console.log('You lose');
-    } else if ((caseFix(playerChoice) == 'Rock' && cpuChoice == 'Scissors') || (caseFix(playerChoice) == 'Scissors' && cpuChoice == 'Paper') || (caseFix(playerChoice) == 'Paper' && cpuChoice == 'Rock')) {
+    } else if ((playerChoice == 'Rock' && cpuChoice == 'Scissors') || (playerChoice == 'Scissors' && cpuChoice == 'Paper') || (playerChoice == 'Paper' && cpuChoice == 'Rock')) {
         console.log('You win!');
     } else {
         console.log('You tie');
