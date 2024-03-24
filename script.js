281a5de478ed646f3aa1b990084b83d430ee26ea
Kerosity <9063443+kerosity@users.noreply.github.com>
2024-03-24 18:34:26 -0500

    Added full functionality with UI for RPS

--- script.js
+++ script.js
@@ -69,41 +69,101 @@ console.log(scoring());*/
 
     playerScore = 0;
     cpuScore = 0;
+    round = 1;
 
-    function game() {
 
+    const btn = document.querySelectorAll('button');
+    const combatLog = document.querySelector('#selections');
+    const cpuPoints = document.querySelector('#cpu-score');
+    const playerPoints = document.querySelector('#your-score');
 
-        function playRound() {
+    btn.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            playRound(e.target.value);
+        });
+    });
 
-        let playerInput = prompt('Choose Your Destiny: Paper, Scissors, or Rock', '');
-        const playerSelection = caseFix(playerInput);
-        const computerSelection = getComputerChoice();        
+        function playRound(playerSelection) {
+
+            console.log('You chose:', playerSelection);
+
+            const combatRound = document.createElement('div');
+            const roundNum = document.createElement('div');
+            roundNum.className = 'round';    
+                        
+            roundNum.textContent = 'Round: ' + round; 
+            combatLog.appendChild(roundNum);      
+
+            const computerSelection = getComputerChoice();        
 
             function getComputerChoice() {
                 const computerChoices = ["Paper", "Scissors", "Rock"];
                 const randomSelection = Math.floor(Math.random()* computerChoices.length);
                 return computerChoices[randomSelection];
             }
-            console.log('The computer chose:', computerSelection);
+            console.log('The computer chose:', computerSelection);  
+
+            const combatRoundResult = document.createElement('div');
+            combatRoundResult.className = 'results';
+
+            if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Scissors' && computerSelection == 'Rock') || (playerSelection == 'Paper' && computerSelection == 'Scissors'))  {
+                console.log('You lose', computerSelection, 'beats', playerSelection);
+                cpuScore++;
+                round++;
+                document.querySelector('#cpu-score h4').innerText = cpuScore;
+                combatRoundResult.textContent = '>> You lost!';     
+
+            } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Rock')) {
+                console.log('You win!');
+                playerScore++;
+                round++;
+                document.querySelector('#your-score h4').innerText = playerScore;
+                combatRoundResult.textContent = '>> You win!';               
+            } else {
+                console.log('You tied. Play again.');
+                combatRoundResult.textContent = '>> TIE! Replay next round.';                  
+                }
+            console.log('Your score:', playerScore, 'Computer Score:', cpuScore); 
+
+
+            combatRound.textContent = 'You chose: ' + playerSelection + '. Bob chose: ' + computerSelection + '.';
+
+            combatLog.appendChild(combatRound);
+            combatLog.appendChild(combatRoundResult);    
+            if (playerScore == 5 || cpuScore == 5) {
+                if (playerScore == 5) {
+                    document.querySelector('#choiceBox div').innerText = 'You win! Game Over.';
+                    createBtn();
+                } else {
+                    document.querySelector('#choiceBox div').innerText = 'You LOSE! Game Over.';
+                    createBtn();
+                }
+            } else {
+
+            }
+            
+            function createBtn() {
+                let refreshButton = document.createElement('input');
+                refreshButton.type = "button";
+                refreshButton.value = "Click To Play Again";
+                refreshButton.className = "refresh";
+                refreshButton.onclick = function() { window.location.reload(); };
+
+                let selectPlacement = document.querySelector('#choiceBox div');
+                selectPlacement.appendChild(refreshButton);
+
+            }
+
+            /*
             
             function caseFix(playerInput) {
                 let firstLetter = playerInput[0].toUpperCase();
                 let lowerCase = playerInput.toLowerCase();
                 return firstLetter + lowerCase.slice(1);
-            }
+            }*/
 
-            if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Scissors' && computerSelection == 'Rock') || (playerSelection == 'Paper' && computerSelection == 'Scissors'))  {
-                console.log('You lose', computerSelection, 'beats', playerSelection);
-                cpuScore++;
-            } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Rock')) {
-                console.log('You win!');
-                playerScore++;
-            } else {
-                console.log('You tied. Play again.');
-                playRound();
-                }
-            console.log('Your score:', playerScore, 'Computer Score:', cpuScore)                
-        }  // your code here!
+               
+        /*}  // your code here!
         console.log(playRound());
 
         if (playerScore == 3 || cpuScore == 3) {
@@ -114,7 +174,6 @@ console.log(scoring());*/
             }           
         } else {
             game();
-        }
+        */}/*
 
-    }
-    console.log(game());
+    console.log(game());*/
